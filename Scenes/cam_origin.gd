62cf94f5a4cda2d9f01ce3c4173b6b9fec8e8473
QuantumV3 <83087109+QuantumV2@users.noreply.github.com>
2024-08-22 09:38:10 +0300

    stuff

--- Scenes/cam_origin.gd
+++ Scenes/cam_origin.gd
@@ -1,13 +1,13 @@
 extends Node2D
 
-const speed = 0.3;
+const speed = 50;
 
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_left"):
+func _process(_delta : float) -> void:
+	if Input.is_action_pressed("ui_left"):
 		global_position.x -= speed;
-	if event.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		global_position.x += speed;
-	if event.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):
 		global_position.y -= speed;
-	if event.is_action_pressed("ui_down"):
-		global_position.y -= speed;
+	if Input.is_action_pressed("ui_down"):
+		global_position.y += speed;
